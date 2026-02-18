f766301556130e8bce2cabac2d4814529138e679
Felix Singer <felixsinger@posteo.net>
2026-02-18 23:16:42 +0100

    qbz: init at 1.2.0
    Signed-off-by: Felix Singer <felixsinger@posteo.net>

---
+++ pkgs/by-name/qb/qbz/package.nix
@@ -0,0 +1,86 @@
+{
+  alsa-lib,
+  cargo-tauri,
+  clang,
+  fetchFromGitHub,
+  fetchNpmDeps,
+  lib,
+  libappindicator,
+  libappindicator-gtk3,
+  libayatana-appindicator,
+  llvmPackages,
+  makeWrapper,
+  nix-update-script,
+  nodejs,
+  npmHooks,
+  openssl,
+  pkg-config,
+  rustPlatform,
+  webkitgtk_4_1,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "qbz";
+  version = "1.2.0";
+
+  src = fetchFromGitHub {
+    owner = "vicrodh";
+    repo = "qbz";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-hiadYCBQn2+YFKam4RJ1ae+1JN5mgEjyQVyGnxr8VPA=";
+  };
+
+  cargoHash = "sha256-GzQ/6oHkJU7KtZqdPR4M4IFJ/C0tKshbOnnnLNYqqf0=";
+  cargoRoot = "src-tauri";
+  buildAndTestSubdir = finalAttrs.cargoRoot;
+
+  npmDeps = fetchNpmDeps {
+    name = "qbz-${finalAttrs.version}-npm-deps";
+    inherit (finalAttrs) src;
+    hash = "sha256-koLJdbtSgDoCiI+u9lhR/iJ+uY63NbVuDkS+ytLp7Bg=";
+  };
+
+  env.LIBCLANG_PATH = "${lib.getLib llvmPackages.libclang}/lib";
+
+  nativeBuildInputs = [
+    cargo-tauri.hook
+    clang
+    makeWrapper
+    nodejs
+    npmHooks.npmConfigHook
+    pkg-config
+  ];
+
+  buildInputs = [
+    alsa-lib
+    libappindicator-gtk3
+    openssl
+    webkitgtk_4_1
+  ];
+
+  doCheck = false;
+
+  postInstall = ''
+    wrapProgram $out/bin/qbz \
+      --prefix LD_LIBRARY_PATH : ${
+        lib.makeLibraryPath [
+          libappindicator
+          libayatana-appindicator
+        ]
+      }
+  '';
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "A native, full-featured hi-fi Qobuz desktop player for Linux, with fast, bit-perfect audio playback";
+    homepage = "https://qbz.lol";
+    changelog = "https://github.com/vicrodh/qbz/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [
+      felixsinger
+    ];
+    mainProgram = "qbz";
+    platforms = lib.platforms.linux;
+  };
+})
